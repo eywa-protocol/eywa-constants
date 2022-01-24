f5ac0f2e0b5d071d76292a26a0f1079b33b9d349
Олексiй <alexnagorny.an@gmail.com>
2022-01-24 11:25:22 +0300

    added abis and tokens

---
+++ src/Blockchain/evm/constants/abis/interfaces.ts
@@ -0,0 +1,5 @@
+import { Interface } from '@ethersproject/abi';
+
+import { ERC20_ABI } from './';
+
+export const ERC20_INTERFACE = new Interface(ERC20_ABI);
--- src/abis/index.ts
+++
@@ -1,2 +0,0 @@
-import relayHubAbi from './relayHubAbi.json'
-export default {relayHubAbi}
